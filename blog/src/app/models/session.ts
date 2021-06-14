fb0f6290d0628b69e58efaf76474049a049afe77
BangPham <bangpham2325@gmail.com>
2021-06-15 00:08:17 +0700

    done 85%

--- blog/src/app/models/session.ts
+++ blog/src/app/models/session.ts
@@ -1,23 +1,16 @@
-import {model, Schema} from 'mongoose';
-interface ISecctionSchema{
-    user:{
-        _id:String,
-        username : String,
-    }
-    lock: {
-        type: Boolean
-    }
-}
-const SessionSchema = new Schema<ISecctionSchema>({
-    user: {
-        _id: String,
-        username: String
-    },
-    lock: {
-        type: Boolean
-    }
+import { Schema, model } from "mongoose";
+
+const SessionSchema = new Schema({
+  user: {
+    _id: String,
+    username: String,
+  },
+  lock: {
+    type: Boolean,
+  },
+  expire_at: { type: Date, default: Date.now, expires: 300 },
 });
 
-const SessionModel = model('sessions', SessionSchema);
+const SessionModel = model("sessions", SessionSchema);
 
-export default SessionModel;
+module.exports = SessionModel;
